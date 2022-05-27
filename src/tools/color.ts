e9f985c428be77f90e58c73c89cdc52f0e5212a8
jeffjing <zgayjjf@qq.com>
2022-05-27 21:31:52 +0800

    feat: change color

--- src/tools/color.ts
+++ src/tools/color.ts
@@ -3,8 +3,8 @@
  * @param account
  */
 export function accountColor (account: string): { color: string, light: string } {
-  const colors = ['#9692E6', '#40C3F0', '#FF9895', '#FEC165', '#E96565', '#3370FF', '#FF4F6E', '#6957ED', '#22C4C6', '#BC51EC', '#FFA86A', '#22C68D']
-  const lights = ['#E8E1FF', '#E1F7FF', '#FFE8E1', '#FFF1E1', '#FFECE1', '#E1E7FF', '#FFE1EA', '#E1E2FF', '#E1FFFF', '#F5E1FF', '#FFECE1', '#E1FFF2']
+  const colors = ['#9692E6', '#40DCFF', '#FF9066', '#FEC165', '#FE749E', '#5BBFFF', '#D57AF1', '#787DFF', '#44DEDE', '#BD76FF', '#FFC844', '#40DAB7']
+  const lights = ['#E8E1FF', '#E1F7FF', '#FFE8E1', '#FFF1E1', '#FFECE1', '#E1E7FF', '#FDE1FF', '#E1E2FF', '#E1FFFF', '#F5E1FF', '#FFECE1', '#E1FFF2']
 
   let index = 0
   for (let i = 0; i < account.length; i++) {
